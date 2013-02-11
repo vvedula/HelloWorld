8121afa18e3e5c26cc0ec5c20e8a211b09f693cb
vvedula <vishnu_vedula@yahoo.co.in>
2013-02-11 14:57:41 -0800

    Added a print statement in testbranch1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 	public static void main(String args[]){
 		System.out.println("Just a test class to configure git");
 		System.out.println("Added a print statement just to make some changes");
+		System.out.println("Added a print statement in the testBranch1");
 		
 		
 	}
