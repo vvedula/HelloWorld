6095639d036d6c7c4e9d91df28e05c35daba658a
vvedula <vishnu_vedula@yahoo.co.in>
2013-02-11 14:40:10 -0800

    Added a new print statement from STS

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,8 @@
 public class HelloWorld {
 	public static void main(String args[]){
 		System.out.println("Just a test class to configure git");
+		System.out.println("Added a print statement just to make some changes");
+		
+		
 	}
 }
